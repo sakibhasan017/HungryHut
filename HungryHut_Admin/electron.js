e35732b25483fbf5616bd3d5ebfe1df2b5a0e1b2
sakibhasan017 <hassansakib512@gmail.com>
2025-07-04 16:19:44 +0600

    Final

---
+++ HungryHut_Admin/electron.js
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  
+  win.loadURL('https://hungry-hut-admin.vercel.app');
+
+
+  
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
